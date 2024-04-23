b301b96b6d2fea4212920cb3d3bce0992950dbb4
Esmat Najafi <145345735+Esmatnajafi@users.noreply.github.com>
2024-04-23 10:59:49 +0200

    style constainer to look like shopping web

--- api-project/src/Components/Form/FormApi.tsx
+++ api-project/src/Components/Form/FormApi.tsx
@@ -2,33 +2,73 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { PhotoApi } from "./PhotoApi";
 
-const FormApi: React.FC = ()=> {
+const FormApi: React.FC = () => {
+  const [newApi, setnewApi] = useState<PhotoApi[]>([]);
 
-    const [newApi, setnewApi] = useState<PhotoApi[]>([]);
+  const [selectedItems, setSelectedItems] = useState<boolean[]>([]);
 
-    useEffect(() => {
-        const getApi = async () =>{ 
-            try {
-            const fetchData = await axios.get('https://fakestoreapi.com/products');
-            setnewApi(fetchData.data);
-        } catch (error) {
-            console.log("error fetching data", error);
-        }
-    }
-        getApi();
-    }, []);
+  useEffect(() => {
+    const getApi = async () => {
+      try {
+        const fetchData = await axios.get("https://fakestoreapi.com/products");
+        setnewApi(fetchData.data);
+      } catch (error) {
+        console.log("error fetching data", error);
+      }
+    };
+    getApi();
+  }, []);
 
-    return(
-      <div className="container">
-        {newApi.map((itemValue) => (
-            <><div className="card" key={itemValue.id}>
-                <h4>{itemValue.title}</h4>
-            </div>
-            <img src={itemValue.image} alt={itemValue.title} /><p>Price: {itemValue.price}</p><p>Description: {itemValue.description}</p><p>Category: {itemValue.category}</p></>
-        ))}
-      </div>
-    );
-}
+  const handleClick = (index: number) => {
+    setSelectedItems((prevState) => {
+      const newState = [...prevState];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  };
 
+  const toggleExpand = (index: number) => {
+    setSelectedItems((prevState) => {
+      const newState = [...prevState];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  };
+
+  return (
+    <div className="container">
+      {newApi.map((itemValue, index) => (
+        <div key={itemValue.id} className="card">
+          <h4>{itemValue.title}</h4>
+          <div className="image-container">
+            <img src={itemValue.image} alt={itemValue.title} />
+          </div>
+          <p>
+            <span>Price:</span> {itemValue.price}
+          </p>
+          <p>
+            <span>Description:</span>{" "}
+            <p className={selectedItems[index] ? "expanded" : ""}>
+              {itemValue.description}
+            </p>
+            {!selectedItems[index] && (
+              <p
+                className="read-more"
+                onClick={() => toggleExpand(index)}
+              >
+                Read More
+              </p>
+            )}
+          </p>
+          <p>
+            <span>Category:</span> {itemValue.category}
+          </p>
+          <button onClick={() => handleClick(index)}>Add</button>
+          {selectedItems[index] && <p>Item selected!</p>}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default FormApi;
